a296e371dee6b3634db5ef5606948c89ce4ae110
afrizaloky <36653795+afrizaloky@users.noreply.github.com>
2019-04-17 09:41:21 +0700

    Update newton.py
    Change derivative method

--- newton.py
+++ newton.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-from sympy import var
-from sympy import sympify
+from sympy import *
 from math import *
-#from tabulate import tabulate
+
 
 #Definition Function
-x = var('x')  # the possible variable names must be known beforehand...
+x = var('x') 
 user_input = input("Enter the function (X**2 + 2*x + 2) = ")
 expr = sympify(user_input)
 x_old = float(input("Enter the value of X0 = "))
@@ -18,12 +17,9 @@ def f(value):
 	return res
 
 def derive(f, value):
-	
-	h= 0.00000000001
-	top = f(value + h) - f(value)
-	bottom = h
-	slope = top/bottom
-	return float("%.3f" % slope)
+	x = Symbol('x')
+	derive = diff(expr,x)
+	return derive.subs(x, value)
 
 def newton(x_old):
 	x_new = x_old - f(x_old)/derive(f, x_old)
@@ -31,11 +27,10 @@ def newton(x_old):
 
 
 print(" \tX\t\tRAE")
+
 while RAE<RAE_now:
 	x_new = newton(x_old)
 	selisih = abs(x_new-x_old)
 	RAE_now = abs(selisih/x_new)
-	print
 	print("", x_new, RAE_now)
-	
 	x_old = x_new
